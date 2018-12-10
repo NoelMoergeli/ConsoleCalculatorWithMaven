c509c3a5bb785487735a02df23baa519dd835449
5ia17nomoergeli <5ia17nomoergeli@gibwintra.ch>
2018-12-10 08:56:30 +0100

    ImplementedMethotsForSummAndDifference

---
+++ src/main/java/ch/bbw/consolecalculator/Calculator.java
@@ -0,0 +1,11 @@
+package ch.bbw.consolecalculator;
+
+public class Calculator {
+
+	public int summe(int summand1, int summand2) {
+		return summand1 + summand2;
+	}
+	public int differenz(int minuend, int subtrahend) {
+		return minuend - subtrahend;
+	}
+}
