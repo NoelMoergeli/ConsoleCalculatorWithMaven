e6cd9f8a772999e14759d7f8f13e1936be51659b
5ia17nomoergeli <5ia17nomoergeli@gibwintra.ch>
2019-01-07 08:20:03 +0100

    ImplementedMethodsForDivisionAndMultiplication

--- src/main/java/ch/bbw/consolecalculator/Calculator.java
+++ src/main/java/ch/bbw/consolecalculator/Calculator.java
@@ -8,4 +8,13 @@ public class Calculator {
 	public int differenz(int minuend, int subtrahend) {
 		return minuend - subtrahend;
 	}
+	public int division(int dividend, int divisor) {
+		if(divisor == 0) {
+			throw new ArithmeticException();
+		}
+		return dividend / divisor;
+	}
+	public int multiplikation(int multiplikator1, int multiplikator2) {
+		return multiplikator1 * multiplikator2;
+	}
 }
