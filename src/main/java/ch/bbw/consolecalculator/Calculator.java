d29e601ac54bb3b12bd01ccce2a477141db4abf6
5ia17nomoergeli <5ia17nomoergeli@gibwintra.ch>
2019-01-14 07:58:58 +0100

    Implemented JUnit Tests for Division

--- src/main/java/ch/bbw/consolecalculator/Calculator.java
+++ src/main/java/ch/bbw/consolecalculator/Calculator.java
@@ -3,18 +3,38 @@ package ch.bbw.consolecalculator;
 public class Calculator {
 
 	public int summe(int summand1, int summand2) {
+		long value = (long) summand1 + (long) summand2;
+		if((value > Integer.MAX_VALUE) || (value < Integer.MIN_VALUE)) {
+			throw new ArithmeticException();
+		}
 		return summand1 + summand2;
 	}
+	
 	public int differenz(int minuend, int subtrahend) {
+		long value = (long) minuend - (long) subtrahend;
+		if((value > Integer.MAX_VALUE) || (value < Integer.MIN_VALUE)) {
+			throw new ArithmeticException();
+		}
 		return minuend - subtrahend;
 	}
+	
 	public int division(int dividend, int divisor) {
 		if(divisor == 0) {
 			throw new ArithmeticException();
 		}
+		
+		long value = (long) dividend / (long) divisor;
+		if((value > Integer.MAX_VALUE) || (value < Integer.MIN_VALUE)) {
+			throw new ArithmeticException();
+		}
 		return dividend / divisor;
 	}
+	
 	public int multiplikation(int multiplikator1, int multiplikator2) {
+		long value = (long) multiplikator1 * (long) multiplikator2;
+		if((value > Integer.MAX_VALUE) || (value < Integer.MIN_VALUE)) {
+			throw new ArithmeticException();
+		}
 		return multiplikator1 * multiplikator2;
 	}
 }
--- src/test/java/ch/bbw/consolecalculator/CalculatorTest.java
+++ src/test/java/ch/bbw/consolecalculator/CalculatorTest.java
@@ -13,6 +13,8 @@ public class CalculatorTest {
 		testee = new Calculator();
 
 	}
+	
+	//ADDITION TESTEN
 	@Test
 	public void test_addition_with_2_positive_numbers() {
 	  assertEquals(4, testee.summe(2, 2));
@@ -25,6 +27,44 @@ public class CalculatorTest {
 	}
 	
 	@Test
+	public void test_addition_with_2_0_numbers() {
+		  assertEquals(0, testee.summe(0, 0));
+			
+	}
+	
+	@Test
+	public void test_addition_with_1_positive_number_and_1_negative_number() {
+		  assertEquals(15, testee.summe(20, -5));
+			
+	}
+	
+	@Test
+	public void test_addition_with_maxValue_OK() {
+		  assertTrue(testee.summe(Integer.MAX_VALUE, 0) == Integer.MAX_VALUE);
+			
+	}
+	
+	@Test
+	public void test_addition_with_minValue_OK() {
+		  assertTrue(testee.summe(Integer.MIN_VALUE, 0) == Integer.MIN_VALUE);
+			
+	}
+	
+	@Test
+	public void test_addition_with_maxValue_NOK() {
+		  assertFalse(testee.summe(Integer.MAX_VALUE, -2) == Integer.MAX_VALUE);
+			
+	}
+	
+	@Test
+	public void test_addition_with_minValue_NOK() {
+		  assertFalse(testee.summe(Integer.MIN_VALUE, 2) == Integer.MIN_VALUE);
+			
+	}
+	
+	
+	//SUBTRAKTION TESTEN
+	@Test
 	public void test_subtraction_with_2_positive_numbers() {
 		  assertEquals(5, testee.differenz(20, 15));
 			
@@ -35,5 +75,92 @@ public class CalculatorTest {
 		  assertEquals(1, testee.differenz(-1, -2));
 			
 	}
+	
+	@Test
+	public void test_subtraction_with_2_0_numbers() {
+		  assertEquals(0, testee.differenz(0, 0));
+			
+	}
+	
+	@Test
+	public void test_subtraction_with_1_positive_number_and_1_negative_number() {
+		  assertEquals(25, testee.differenz(20, -5));
+			
+	}
+	
+	@Test
+	public void test_subtraction_with_maxValue_OK() {
+		  assertTrue(testee.differenz(Integer.MAX_VALUE, 0) == Integer.MAX_VALUE);
+			
+	}
+	
+	@Test
+	public void test_subtraction_with_minValue_OK() {
+		  assertTrue(testee.differenz(Integer.MIN_VALUE, 0) == Integer.MIN_VALUE);
+			
+	}
+	
+	@Test
+	public void test_subtraction_with_maxValue_NOK() {
+		  assertFalse(testee.differenz(Integer.MAX_VALUE, 2) == Integer.MAX_VALUE);
+			
+	}
+	
+	@Test
+	public void test_subtraction_with_minValue_NOK() {
+		  assertFalse(testee.differenz(Integer.MIN_VALUE, -2) == Integer.MIN_VALUE);
+			
+	}
+	
+	//DIVISION TESTEN
+	
+	@Test
+	public void test_division_with_2_positive_numbers() {
+		  assertEquals(10, testee.division(20, 2));
+			
+	}
+	
+	@Test
+	public void test_division_with_2_negative_numbers() {
+		  assertEquals(-10, testee.division(-20, -2));
+			
+	}
+	
+	@Test(expected = ArithmeticException.class)
+	public void test_division_with_2_0_numbers() {
+		  assertEquals(0, testee.division(0, 0));
+			
+	}
+	
+	@Test
+	public void test_division_with_1_positive_number_and_1_negative_number() {
+		  assertEquals(-4, testee.division(20, -5));
+			
+	}
+	
+	@Test
+	public void test_division_with_maxValue_OK() {
+		  assertTrue(testee.division(Integer.MAX_VALUE, 0) == Integer.MAX_VALUE);
+			
+	}
+	
+	@Test
+	public void test_division_with_minValue_OK() {
+		  assertTrue(testee.division(Integer.MIN_VALUE, 0) == Integer.MIN_VALUE);
+			
+	}
+	
+	@Test
+	public void test_division_with_maxValue_NOK() {
+		  assertFalse(testee.division(Integer.MAX_VALUE, 2) == Integer.MAX_VALUE);
+			
+	}
+	
+	@Test
+	public void test_division_with_minValue_NOK() {
+		  assertFalse(testee.division(Integer.MIN_VALUE, -2) == Integer.MIN_VALUE);
+			
+	}
+	//MULTIPLIKATION TESTEN
 
 }
